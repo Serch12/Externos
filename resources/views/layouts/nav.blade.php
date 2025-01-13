e033190742e6dad43626f06955c25b870c2f242c
Serch12 <sergio@amfpro.mx>
2025-01-13 13:33:34 -0600

    modificacion de perfil he fotos perfil

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -286,10 +286,10 @@
           <li class="nav-item navbar-dropdown dropdown-user dropdown">
             <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
               <div class="avatar avatar-online">
-                <img src="{{ asset('ArchivosSistema/Documentacion/'.$imagen->foto) }}" 
-                  alt="Avatar" 
-                  class="rounded-circle" 
-                  onerror="this.onerror=null; this.src='{{ asset('style/logos/sinfoto.png') }}';" />
+                <img src="{{ asset('ArchivosSistema/Documentacion/'. optional($imagen)->foto) }}" 
+                alt="Avatar" 
+                class="rounded-circle" 
+                onerror="this.onerror=null; this.src='{{ asset('ruta/a/imagen/alternativa.jpg') }}';" />
             </div>
             </a>
             <ul class="dropdown-menu dropdown-menu-end">
