65831c8d552911880fe39f4090ab69b63fccca34
Serch12 <sergio@amfpro.mx>
2025-05-12 16:14:37 -0600

    cambios para activas los extras de talentos

--- app/Http/Controllers/HonorariosController.php
+++ app/Http/Controllers/HonorariosController.php
@@ -80,7 +80,7 @@ class HonorariosController extends Controller
 
         
 
-        if ($hoy->day === 12) {
+        if ($hoy->day === 1) {
             $new = new Honorarios();
             $new ->id_usuario = 6;
             $new -> nombre_usuario = 'Angelica Cabrera';
@@ -111,7 +111,7 @@ class HonorariosController extends Controller
 
         }
 
-        if ($hoy->day === 10) {
+        if ($hoy->day === 15) {
             // Cambiar el estatus si existe
             $mesActual = Carbon::now()->format('m');
             $añoActual = Carbon::now()->format('Y');
