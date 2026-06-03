0aee4c8ece6d03725e5a3039c4c34eebeea636b4
Arturo Marin <arturo@rehabevolution.com>
2026-06-03 10:57:50 -0600

    cambios en externos nuevas fechas

--- app/Http/Controllers/HonorariosController.php
+++ app/Http/Controllers/HonorariosController.php
@@ -111,7 +111,7 @@ class HonorariosController extends Controller
 
         }
 
-        if ($hoy->day === 13) {
+        if ($hoy->day === 10) {
             // Cambiar el estatus si existe
             $mesActual = Carbon::now()->format('m');
             $añoActual = Carbon::now()->format('Y');
@@ -190,7 +190,7 @@ class HonorariosController extends Controller
 
         }
 
-        if ($hoy->day === 13) {
+        if ($hoy->day === 10) {
             // Cambiar el estatus si existe
             $mesActual = Carbon::now()->format('m');
             $añoActual = Carbon::now()->format('Y');
